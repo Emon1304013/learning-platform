2f31212e734f298edfefd8fd8d2a4119ff73f282
KamalEmon <u1304013@student.cuet.ac.bd>
2022-10-25 16:57:54 +0600

    Course Card name onclick route created to Course details page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
 import Blog from "../pages/Blog/Blog";
+import CourseDetails from "../pages/CourseDetails/CourseDetails";
 import Courses from "../pages/Courses/Courses";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Home from "../pages/Home/Home";
@@ -24,10 +25,11 @@ const router = createBrowserRouter([
                element:<Courses></Courses>,
                loader: ()=> fetch('http://localhost:5000/courses') 
             },
-            // {
-            //     path:'/courses/:id',
-            //     element:
-            // },
+            {
+                path:'/courses/:id',
+                element:<CourseDetails></CourseDetails>,
+                loader:({params}) => fetch(`http://localhost:5000/courses/{params.id}`)
+            },
             {
                 path:'/blog',
                 element:<Blog></Blog>
